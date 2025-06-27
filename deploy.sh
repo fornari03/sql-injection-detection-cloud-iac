32ed4b2e0e86a46996e9808cdbe9daa4577f0ce0
fornari03 <221017023@aluno.unb.br>
2025-06-26 21:31:17 -0300

    feat: cria deploy.sh pra provisionar e configurar a infraestrutura na nuvem

---
+++ deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+# Check if the user provided the path to the private key
+if [[ $# -ne 1 ]]; then
+  echo "Missing argument: Path to the private key file is required."
+  exit 1
+fi
+
+KEY_PATH="$1"
+
+# Check if the provided key file exists
+if [[ ! -f "$KEY_PATH" ]]; then
+  echo "Error: private key file not found in '$KEY_PATH'"
+  exit 1
+fi
+
+
+
+
+
+# 1. Apply Terraform configuration
+terraform apply -var-file=variables.tfvars -auto-approve
+
+
+# 2. Get the public IPs of the VMs
+VM_DATABASE_IP=$(terraform output -raw vm_db_ip)
+
+
+# 3. Create the Ansible inventory file
+cat > hosts <<EOF
+[database]
+${VM_DATABASE_IP}
+EOF
+
+echo "'hosts' file generated:"
+cat hosts
+
+
+# 4. Create the Ansible configuration file
+cat > ansible.cfg <<EOF
+[defaults]
+host_key_checking = False
+remote_user = ubuntu
+private_key_file = ${KEY_PATH}
+EOF
+
+echo "'ansible.cfg' file generated"
+cat ansible.cfg
+
+
+# 5. Runs Ansible playbook
+ansible-playbook -i hosts playbook.yaml
